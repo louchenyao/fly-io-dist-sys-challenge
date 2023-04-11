f0ce1fee0fe4b95c5bfea9c0ae59b5c691f8e9e6
Chenyao Lou <louchenyao@gmail.com>
2023-04-11 19:33:32 +0000

    complete challenge 4

--- 4-maelstrom-counter/main.go
+++ 4-maelstrom-counter/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -23,22 +24,47 @@ func main() {
 			old := Value{0, 0}
 			_ = kv.ReadInto(ctx, "counter", &old)
 			new := Value{old.Value + delta, old.Version + 1}
-
 			if err := kv.CompareAndSwap(ctx, "counter", old, new, true); err != nil {
 				continue
 			}
+			_ = kv.Write(ctx, n.ID(), new) // update the local cache
 			break
 		}
 	}
 
-	read := func() int {
+	read := func() Value {
 		ctx := context.Background()
-		v := Value{0, 0}
-		_ = kv.ReadInto(ctx, "counter", &v)
-		// it will return 0 if the key doesn't exist
-		// it's ok since the system is eventually consistent
-		return v.Value
+		global_v := Value{0, 0}
+		local_v := Value{0, 0}
+		_ = kv.ReadInto(ctx, "counter", &global_v)
+		_ = kv.ReadInto(ctx, n.ID(), &local_v)
+		// return the value with the highest version
+		if local_v.Version > global_v.Version {
+			return local_v
+		}
+		return global_v
 	}
+
+	send_all := func(v Value) {
+		// broadcast the value to all nodes
+		for _, node := range n.NodeIDs() {
+			if node == n.ID() {
+				continue
+			}
+			n.Send(node, map[string]any{
+				"type":  "update",
+				"value": v,
+			})
+		}
+	}
+
+	// broadcast the value to all nodes every second
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			send_all(read())
+		}
+	}()
 
 	n.Handle("add", func(msg maelstrom.Message) error {
 		message := struct {
@@ -58,8 +84,25 @@ func main() {
 	n.Handle("read", func(msg maelstrom.Message) error {
 		return n.Reply(msg, map[string]any{
 			"type":  "read_ok",
-			"value": read(),
+			"value": read().Value,
 		})
+	})
+
+	n.Handle("update", func(msg maelstrom.Message) error {
+		message := struct {
+			Value Value
+		}{}
+		if err := json.Unmarshal(msg.Body, &message); err != nil {
+			return err
+		}
+
+		// update the local cache
+		local := read()
+		if message.Value.Version > local.Version {
+			_ = kv.Write(context.Background(), n.ID(), message.Value)
+		}
+
+		return nil
 	})
 
 	if err := n.Run(); err != nil {
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Repo for [fly.io dist-sys challenge](https://fly.io/dist-sys/)
 ### Challenge 4
 
 My first implementation is to use `seq-kv` with a single key `counter`. All add requests will increment the counter in the lock-free manner (using `cas` operation). And read requests will simply read the counter. It failed the test because [sequential consistency](https://jepsen.io/consistency/models/sequential) does not gurentee that all of nodes see the latest state. "A process in a sequentially consistent system may be far ahead, or behind, of other processes" quote from [sequential consistency](https://jepsen.io/consistency/models/sequential).
+
+Using lineerizable kv store `lin-kv` is too overkill for this challenge. So I added the broadcast mechanism which will broadcast the latest counter value to all neighbors periodically. And the read request will simply read the counter from the local cache.
+
+The challenge does not specify "how long" is the "eventual". In theory, the first implementation is technically correct if the "eventual" is infinite.  I found that the test actually waits 10 seconds before the final check. So setting the broadcast interval to 1 seconds is enough to pass the test.
